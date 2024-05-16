4e37e377c067872bfe3e3217ecd3bebc5be4d164
Mykola Burakov <burakov.work@gmail.com>
2024-05-17 00:59:10 +0300

    upd

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,348 +1,348 @@
 import type { PermissionState } from '@capacitor/core';
 
 export interface PermissionStatus {
-  contacts: PermissionState;
+    contacts: PermissionState;
 }
 
 export interface ContactsPlugin {
-  checkPermissions(): Promise<PermissionStatus>;
-  requestPermissions(): Promise<PermissionStatus>;
-  getContact(options: GetContactOptions): Promise<GetContactResult>;
-  getContacts(options: GetContactsOptions): Promise<GetContactsResult>;
-  createContact(options: CreateContactOptions): Promise<CreateContactResult>;
-  deleteContact(options: DeleteContactOptions): Promise<void>;
-  pickContact(options: PickContactOptions): Promise<PickContactResult>;
+    checkPermissions(): Promise<PermissionStatus>;
+    requestPermissions(): Promise<PermissionStatus>;
+    getContact(options: GetContactOptions): Promise<GetContactResult>;
+    getContacts(options: GetContactsOptions): Promise<GetContactsResult>;
+    createContact(options: CreateContactOptions): Promise<CreateContactResult>;
+    deleteContact(options: DeleteContactOptions): Promise<void>;
+    pickContact(options: PickContactOptions): Promise<PickContactResult>;
 }
 
 export enum PhoneType {
-  // Home / Work
-  Home = 'home',
-  Work = 'work',
-  // Other / Custom
-  Other = 'other',
-  Custom = 'custom',
-  // Phone specific
-  Mobile = 'mobile',
-  FaxWork = 'fax_work',
-  FaxHome = 'fax_home',
-  Pager = 'pager',
-  Callback = 'callback',
-  Car = 'car',
-  CompanyMain = 'company_main',
-  Isdn = 'isdn',
-  Main = 'main',
-  OtherFax = 'other_fax',
-  Radio = 'radio',
-  Telex = 'telex',
-  TtyTdd = 'tty_tdd',
-  WorkMobile = 'work_mobile',
-  WorkPager = 'work_pager',
-  Assistant = 'assistant',
-  Mms = 'mms',
+    // Home / Work
+    Home = 'home',
+    Work = 'work',
+    // Other / Custom
+    Other = 'other',
+    Custom = 'custom',
+    // Phone specific
+    Mobile = 'mobile',
+    FaxWork = 'fax_work',
+    FaxHome = 'fax_home',
+    Pager = 'pager',
+    Callback = 'callback',
+    Car = 'car',
+    CompanyMain = 'company_main',
+    Isdn = 'isdn',
+    Main = 'main',
+    OtherFax = 'other_fax',
+    Radio = 'radio',
+    Telex = 'telex',
+    TtyTdd = 'tty_tdd',
+    WorkMobile = 'work_mobile',
+    WorkPager = 'work_pager',
+    Assistant = 'assistant',
+    Mms = 'mms',
 }
 
 export enum EmailType {
-  // Home / Work
-  Home = 'home',
-  Work = 'work',
-  // Other / Custom
-  Other = 'other',
-  Custom = 'custom',
-  // Email specific
-  Mobile = 'mobile',
+    // Home / Work
+    Home = 'home',
+    Work = 'work',
+    // Other / Custom
+    Other = 'other',
+    Custom = 'custom',
+    // Email specific
+    Mobile = 'mobile',
 }
 
 export enum PostalAddressType {
-  // Home / Work
-  Home = 'home',
-  Work = 'work',
-  // Other / Custom
-  Other = 'other',
-  Custom = 'custom',
+    // Home / Work
+    Home = 'home',
+    Work = 'work',
+    // Other / Custom
+    Other = 'other',
+    Custom = 'custom',
 }
 
 export interface Projection {
-  /**
-   * @default false
-   */
-  name?: boolean;
+    /**
+     * @default false
+     */
+    name?: boolean;
 
-  /**
-   * @default false
-   */
-  organization?: boolean;
+    /**
+     * @default false
+     */
+    organization?: boolean;
 
-  /**
-   * @default false
-   */
-  birthday?: boolean;
+    /**
+     * @default false
+     */
+    birthday?: boolean;
 
-  /**
-   * @default false
-   */
-  note?: boolean;
+    /**
+     * @default false
+     */
+    note?: boolean;
 
-  /**
-   * @default false
-   */
-  phones?: boolean;
+    /**
+     * @default false
+     */
+    phones?: boolean;
 
-  /**
-   * @default false
-   */
-  emails?: boolean;
+    /**
+     * @default false
+     */
+    emails?: boolean;
 
-  /**
-   * @default false
-   */
-  urls?: boolean;
+    /**
+     * @default false
+     */
+    urls?: boolean;
 
-  /**
-   * @default false
-   */
-  postalAddresses?: boolean;
+    /**
+     * @default false
+     */
+    postalAddresses?: boolean;
 
-  /**
-   * Be careful! This can potentially slow down your query by a large factor.
-   *
-   * @default false
-   */
-  image?: boolean;
+    /**
+     * Be careful! This can potentially slow down your query by a large factor.
+     *
+     * @default false
+     */
+    image?: boolean;
 }
 
 export interface GetContactOptions {
-  contactId: string;
-  projection: Projection;
+    contactId: string;
+    projection: Projection;
 }
 
 export interface GetContactResult {
-  contact: ContactPayload;
+    contact: ContactPayload;
 }
 
 export interface GetContactsOptions {
-  projection: Projection;
+    projection: Projection;
 }
 
 export interface GetContactsResult {
-  contacts: ContactPayload[];
+    contacts: ContactPayload[];
 }
 
 export interface NamePayload {
-  display: string | null;
-  given: string | null;
-  middle: string | null;
-  family: string | null;
-  prefix: string | null;
-  suffix: string | null;
+    display: string | null;
+    given: string | null;
+    middle: string | null;
+    family: string | null;
+    prefix: string | null;
+    suffix: string | null;
 }
 
 export interface OrganizationPayload {
-  company: string | null;
-  jobTitle: string | null;
-  department: string | null;
+    company: string | null;
+    jobTitle: string | null;
+    department: string | null;
 }
 
 export interface BirthdayPayload {
-  day?: number | null;
-  month?: number | null;
-  year?: number | null;
+    day?: number | null;
+    month?: number | null;
+    year?: number | null;
 }
 
 export interface PhonePayload {
-  type: PhoneType;
-  label?: string | null;
-  isPrimary?: boolean | null;
+    type: PhoneType;
+    label?: string | null;
+    isPrimary?: boolean | null;
 
-  number: string | null;
+    number: string | null;
 }
 
 export interface EmailPayload {
-  type: EmailType;
-  label?: string | null;
-  isPrimary?: boolean | null;
+    type: EmailType;
+    label?: string | null;
+    isPrimary?: boolean | null;
 
-  address: string | null;
+    address: string | null;
 }
 
 export interface PostalAddressPayload {
-  type: PostalAddressType;
-  label?: string | null;
-  isPrimary?: boolean | null;
+    type: PostalAddressType;
+    label?: string | null;
+    isPrimary?: boolean | null;
 
-  street?: string | null;
-  neighborhood?: string | null;
-  city?: string | null;
-  region?: string | null;
-  postcode?: string | null;
-  country?: string | null;
+    street?: string | null;
+    neighborhood?: string | null;
+    city?: string | null;
+    region?: string | null;
+    postcode?: string | null;
+    country?: string | null;
 }
 
 export interface ImagePayload {
-  base64String?: string | null;
+    base64String?: string | null;
 }
 
 export interface ContactPayload {
-  contactId: string;
+    contactId: string;
 
-  /**
-   * Object holding the name data
-   */
-  name?: NamePayload;
+    /**
+     * Object holding the name data
+     */
+    name?: NamePayload;
 
-  /**
-   * Object holding the organization data
-   */
-  organization?: OrganizationPayload;
+    /**
+     * Object holding the organization data
+     */
+    organization?: OrganizationPayload;
 
-  /**
-   * Birthday
-   */
-  birthday?: BirthdayPayload | null;
+    /**
+     * Birthday
+     */
+    birthday?: BirthdayPayload | null;
 
-  /**
-   * Note
-   */
-  note?: string | null;
+    /**
+     * Note
+     */
+    note?: string | null;
 
-  /**
-   * Phones
-   */
-  phones?: PhonePayload[];
+    /**
+     * Phones
+     */
+    phones?: PhonePayload[];
 
-  /**
-   * Emails
-   */
-  emails?: EmailPayload[];
+    /**
+     * Emails
+     */
+    emails?: EmailPayload[];
 
-  /**
-   * URLs
-   */
-  urls?: (string | null)[];
+    /**
+     * URLs
+     */
+    urls?: (string | null)[];
 
-  /**
-   * Postal Addresses
-   */
-  postalAddresses?: PostalAddressPayload[];
+    /**
+     * Postal Addresses
+     */
+    postalAddresses?: PostalAddressPayload[];
 
-  /**
-   * Image
-   */
-  image?: ImagePayload;
+    /**
+     * Image
+     */
+    image?: ImagePayload;
 }
 
 export interface CreateContactOptions {
-  contact: ContactInput;
+    contact: ContactInput;
 }
 
 export interface NameInput {
-  given?: string | null;
-  middle?: string | null;
-  family?: string | null;
-  prefix?: string | null;
-  suffix?: string | null;
+    given?: string | null;
+    middle?: string | null;
+    family?: string | null;
+    prefix?: string | null;
+    suffix?: string | null;
 }
 
 export interface OrganizationInput {
-  company?: string | null;
-  jobTitle?: string | null;
-  department?: string | null;
+    company?: string | null;
+    jobTitle?: string | null;
+    department?: string | null;
 }
 
 export interface BirthdayInput {
-  day: number;
-  month: number;
-  year?: number;
+    day: number;
+    month: number;
+    year?: number;
 }
 
 export interface PhoneInput {
-  type: PhoneType;
-  label?: string | null;
-  isPrimary?: boolean;
+    type: PhoneType;
+    label?: string | null;
+    isPrimary?: boolean;
 
-  number: string | null;
+    number: string | null;
 }
 
 export interface EmailInput {
-  type: EmailType;
-  label?: string | null;
-  isPrimary?: boolean;
+    type: EmailType;
+    label?: string | null;
+    isPrimary?: boolean;
 
-  address: string | null;
+    address: string | null;
 }
 
 export interface PostalAddressInput {
-  type: PostalAddressType;
-  label?: string | null;
-  isPrimary?: boolean;
+    type: PostalAddressType;
+    label?: string | null;
+    isPrimary?: boolean;
 
-  street?: string | null;
-  neighborhood?: string | null;
-  city?: string | null;
-  region?: string | null;
-  postcode?: string | null;
-  country?: string | null;
+    street?: string | null;
+    neighborhood?: string | null;
+    city?: string | null;
+    region?: string | null;
+    postcode?: string | null;
+    country?: string | null;
 }
 
 export interface ContactInput {
-  /**
-   * Object holding the name data
-   */
-  name?: NameInput;
+    /**
+     * Object holding the name data
+     */
+    name?: NameInput;
 
-  /**
-   * Object holding the organization data
-   */
-  organization?: OrganizationInput;
+    /**
+     * Object holding the organization data
+     */
+    organization?: OrganizationInput;
 
-  /**
-   * Birthday
-   */
-  birthday?: BirthdayInput | null;
+    /**
+     * Birthday
+     */
+    birthday?: BirthdayInput | null;
 
-  /**
-   * Note
-   */
-  note?: string | null;
+    /**
+     * Note
+     */
+    note?: string | null;
 
-  /**
-   * Phones
-   */
-  phones?: PhoneInput[];
+    /**
+     * Phones
+     */
+    phones?: PhoneInput[];
 
-  /**
-   * Emails
-   */
-  emails?: EmailInput[];
+    /**
+     * Emails
+     */
+    emails?: EmailInput[];
 
-  /**
-   * URLs
-   */
-  urls?: string[];
+    /**
+     * URLs
+     */
+    urls?: string[];
 
-  /**
-   * Postal Addresses
-   */
-  postalAddresses?: PostalAddressInput[];
+    /**
+     * Postal Addresses
+     */
+    postalAddresses?: PostalAddressInput[];
 
-  /**
-   * Image
-   */
-  // @TODO:
-  // image?: ImageInput | null;
+    /**
+     * Image
+     */
+    // @TODO:
+    // image?: ImageInput | null;
 }
 
 export interface CreateContactResult {
-  contactId: string;
+    contactId: string;
 }
 
 export interface DeleteContactOptions {
-  contactId: string;
+    contactId: string;
 }
 
 export interface PickContactOptions {
-  projection: Projection;
+    projection: Projection;
 }
 
 export interface PickContactResult {
-  contact: ContactPayload;
+    contact: ContactPayload;
 }
